8a35728a27a12c0a3a42a750ce8ef8a977988065
chae0 <limchae098@gmail.com>
2023-06-23 10:20:40 +0900

    FEAT : add show like list by user, also admin; 회원병 상품 구현

--- src/main/java/com/sparta/myselectshop/service/ProductService.java
+++ src/main/java/com/sparta/myselectshop/service/ProductService.java
@@ -4,8 +4,10 @@ import com.sparta.myselectshop.dto.ProductMypriceRequestDto;
 import com.sparta.myselectshop.dto.ProductRequestDto;
 import com.sparta.myselectshop.dto.ProductResponseDto;
 import com.sparta.myselectshop.entity.Product;
+import com.sparta.myselectshop.entity.User;
 import com.sparta.myselectshop.naver.dto.ItemDto;
 import com.sparta.myselectshop.repository.ProductRepository;
+import com.sparta.myselectshop.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,13 +23,19 @@ public class ProductService {
 
     public static final int MIN_MY_PRICE = 100; // myPrice 가 100 이상이여야 하는 조건을 판별할 상수
 
-    public ProductResponseDto createProduct(ProductRequestDto requestDto) {
+    
+    
+    // 관심 상품으로 등록하기 위해 product Table에 저장하는 메서드
+    public ProductResponseDto createProduct(ProductRequestDto requestDto, User user) {
         // 생성 요청 받아온 데이터를 Entity 객체로 만들어 주고, DB 에도 저장한다.
-        Product product = productRepository.save(new Product(requestDto));
+        Product product = productRepository.save(new Product(requestDto,user));
         return new ProductResponseDto(product); // 브라우저에 response 를 보내기 위해 Dto 타입다시 생성
-    } // 관심 상품으로 등록하기 위해 product Table에 저장하는 메서드
+    } 
 
 
+    
+    
+    // myPrice 칼럼 수정하는 메서드
     // 수정을 할 경우에는 반드시 Transactional 환경이여야 한다.
     // 그래야 spring JPA 에서 변경 감지가 되어서 DB에 내용도 같이 수정합니다.
     @Transactional
@@ -49,9 +57,12 @@ public class ProductService {
         return new ProductResponseDto(product);
     }
 
+    
+    
 
-    public List<ProductResponseDto> getProducts() {
-        List<Product> productList = productRepository.findAll();
+    // 해당 유저가 관심 상품으로 등록한 목록 출력하는 메서드
+    public List<ProductResponseDto> getProducts(User user) {
+        List<Product> productList = productRepository.findAllByUser(user);
         List<ProductResponseDto> responseDtoList = new ArrayList<>();
 
         for (Product product : productList) {
@@ -61,6 +72,9 @@ public class ProductService {
         return responseDtoList;
     }
 
+
+
+    // 해당 상품 데이터 찾아서 lPrice 수정하는 메서드
     @Transactional
     public void updateBySearch(Long id, ItemDto itemDto) {
         Product product = productRepository.findById(id).orElseThrow(
@@ -68,5 +82,19 @@ public class ProductService {
         );
 
         product.updateByItemDto(itemDto);
+    }
+
+
+
+    // 관리자가 들어왔을 경우에는 모든 상품 데이터 조회가 가능한 메서드
+    public List<ProductResponseDto> getAllProducts() {
+        List<Product> productList = productRepository.findAll();
+        List<ProductResponseDto> responseDtoList = new ArrayList<>();
+
+        for (Product product : productList) {
+            responseDtoList.add(new ProductResponseDto(product));
+        }
+
+        return responseDtoList;
     }
 }
